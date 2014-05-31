91eea66400ec8e1a8d3e6b4ebbfdc4a4c3dabb9c
Kelley Stephens <kelley.stephens@outlook.com>
2014-05-31 12:52:25 -0500

    changed map color

--- app/static/js/es6/maps.es6.js
+++ app/static/js/es6/maps.es6.js
@@ -9,7 +9,7 @@
   var map;
 
   function init(){
-    initMap(36, -86, 2);
+    initMap(36, -86, 8);
   }
 
   function addMarker(lat, lng, name){
@@ -18,7 +18,7 @@
   }
 
   function initMap(lat, lng, zoom){
-    let styles = [{'featureType':'water','elementType':'geometry','stylers':[{'color':'#ffdfa6'}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#b52127'}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#c5531b'}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#74001b'},{'lightness':-10}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#da3c3c'}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#74001b'}]},{'featureType':'road.arterial','elementType':'geometry.stroke','stylers':[{'color':'#da3c3c'}]},{'featureType':'road.local','elementType':'geometry.fill','stylers':[{'color':'#990c19'}]},{'elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'elementType':'labels.text.stroke','stylers':[{'color':'#74001b'},{'lightness':-8}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#6a0d10'},{'visibility':'on'}]},{'featureType':'administrative','elementType':'geometry','stylers':[{'color':'#ffdfa6'},{'weight':0.4}]},{'featureType':'road.local','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]}];
+    let styles = [{'featureType':'water','elementType':'geometry','stylers':[{'color':'#a2daf2'}]}];
     let mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, styles: styles};
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
