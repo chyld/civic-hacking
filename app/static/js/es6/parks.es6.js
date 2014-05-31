5b429db4ca5cdd09e89a1a8eb88f3a6dc9a8af82
Jay Politzer <jmpolitzer@gmail.com>
2014-05-31 13:07:05 -0500

    create distance calculator

--- app/static/js/es6/parks.es6.js
+++ app/static/js/es6/parks.es6.js
@@ -12,7 +12,24 @@
 
   function getParksData() {
     var url = 'http://data.nashville.gov/resource/74d7-b74t.json?';
-    $.getJSON(url, addParksDataToMap);
+    $.getJSON(url, findClosestParks);
+  }
+
+  function findClosestParks(data) {
+    var parks;
+    if loc.lat {
+      parks = [];
+      $.each(data, function(i, entry) {
+        var dist = window.getDistance(loc.lat, loc.lng, entry.mapped_location.latitude, entry.mapped_location.longitude);
+        if(dist <= 2) {
+          parks.push(entry);
+        }
+      });
+      console.log(parks.count);
+    } else {
+      parks = data;
+    }
+    addParksDataToMap(parks);
   }
 
   function addParksDataToMap(data) {
