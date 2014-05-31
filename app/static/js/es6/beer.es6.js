80da347521ff728b034c7a3097f011f5e25f048a
Matt Lummus <matthew.w.lummus@gmail.com>
2014-05-31 12:55:35 -0500

    Get beer on click, not document ready

--- app/static/js/es6/beer.es6.js
+++ app/static/js/es6/beer.es6.js
@@ -6,12 +6,15 @@
   $(document).ready(initialize);
 
   function initialize(){
+    $('#get-beer').on('click', getBeerData);
+  }
+
+  function getBeerData(){
     var url = 'http://data.nashville.gov/resource/3wb6-xy3j?permit_type=ON-SALE BEER';
     $.getJSON(url, receive);
   }
 
   function receive(data){
-    console.log(data[0]);
     data.forEach(plotOnMap);
   }
 
