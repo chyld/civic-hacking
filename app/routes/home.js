44c2bd6e1f28b233b90a730faa087d810d9fc5b5
Samantha Yeargin <slyeargin@gmail.com>
2014-06-01 15:04:52 -0500

    Changedpage title from Node.js: Home to iTNerary

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.index = (req, res)=>{
-  res.render('home/index', {title: 'Node.js: Home'});
+  res.render('home/index', {title: 'iTNerary'});
 };
 
 exports.help = (req, res)=>{
